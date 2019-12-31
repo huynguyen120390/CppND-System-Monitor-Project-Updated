5fcc121299804249d4503ae152d810bb64aa0877
Huy Nguyen <huynguyen120390@gmail.com>
2019-12-31 14:22:10 -0800

    Fixed time format

--- src/format.cpp
+++ src/format.cpp
@@ -12,12 +12,18 @@ using std::string;
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) { 
     long hour, min, sec;
-    string time ;
+    string time,hourS,minS,secS ;
 
     hour = seconds/3600;
     min =  (long)((seconds/3600.0 - hour)*60);
     sec =  seconds - min*60 - hour*3600;
-    time = to_string(hour) + ":" + to_string(min) + ":" + to_string(sec);
+    hourS = to_string(hour);
+    minS = to_string(min);
+    secS = to_string(sec);
+    if(hourS.size() == 1) hourS = "0" + hourS;
+    if(minS.size() == 1) minS = "0" + minS;
+    if(secS.size() == 1) secS = "0" + secS;
+    time = hourS + ":" + minS + ":" + secS + '\n' ;
     return time;
 }
 // int main(){
